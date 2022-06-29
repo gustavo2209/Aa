167ca15637dee8e75efbaa849a7d14d157e2a3c8
Gustavo Chimoy Garcia <gustavo_fred@hotmail.com>
2022-06-28 20:42:58 -0500

    actualizaciones

--- Aa/Controllers/HomeController.cs
+++ Aa/Controllers/HomeController.cs
@@ -4,10 +4,51 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Aa.Models;
+using Algoritmos;
+
 namespace Aa.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
+        public JsonResult Autentica(string usuario, string password)
+        {
+            Mensaje mensaje = new Mensaje();
+
+            if(usuario!=null && password != null && usuario.Trim().Length > 0 && password.Trim().Length > 0)
+            {
+                string password_encripted = Aes.Encrypt(password);
+
+                DaoUsuarios daoUsuarios = new DaoUsuarios();
+                usuarios user = daoUsuarios.Autentica(usuario, password_encripted);
+
+                if(user != null)
+                {
+                    Session["user"] = user;
+                    Session["ID"] = Session.SessionID;
+
+                }
+                else
+                {
+                    mensaje.msg = "Usuario NO Registrado";
+                    mensaje.estado = false;
+                }
+            }
+            else
+            {
+                mensaje.msg = "Debe ingresar Usuario y Password";
+                mensaje.estado = false;
+            }
+
+            return Json(mensaje, JsonRequestBehavior.AllowGet);
+        }
+
+        private class Mensaje{
+            public string msg { get; set; }
+            public bool estado { get; set; }
+        }
+
         public ActionResult Index()
         {
             return View();
---
+++ Aa/Models/DaoUsuarios.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aa.Models
+{
+    public class DaoUsuarios
+    {
+        public usuarios Autentica(string usuario, string password)
+        {
+            usuarios user = null;
+
+            using (var db=new ModelAa())
+            {
+                var query = from u in db.usuarios
+                            where u.usuario == usuario && u.password == password
+                            select u;
+
+                if(query.Count() > 0)
+                {
+                    user = query.First();
+                }
+            }
+            
+            return user;
+        }
+    }
+}
